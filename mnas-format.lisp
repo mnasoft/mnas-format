285f17b33a8ee71be537585c791c3ac9d03babd8
Nick Matvyeyev <mnasoft@gmail.com>
2019-07-17 16:34:54 +0300

    2019-7-17 16:34:32 KO11-118383

--- mnas-format.lisp
+++ mnas-format.lisp
@@ -40,3 +40,16 @@
     ((and (numberp a ))                    (format out "~A" (string-trim " " (format nil "~,3E" a))))
     (T                                     (format out "~A"    a))))
 
+(export 'round-val-10)
+(defun round-val-10 (a &optional (out nil))
+  "Пример использования:
+ (loop for i in '(1000000 100000 10000 1000 100 10 1 0.1 0.01  ) do
+   (format t \"~A~%\" (mnas-format:round-val (* i 0.1234567891))))
+"
+  (cond
+    ((null a)                              (format out "" ))
+    ((and (numberp a ) (= a 0))            (format out "0" ))
+    ((and (numberp a ) (< (abs a) 10))     (format out "~A" (string-trim " " (format nil "~7,3F" a))))
+    ((and (numberp a ) (< (abs a) 10000))  (format out "~A" (round a)))
+    ((and (numberp a ))                    (format out "~A" (string-trim " " (format nil "~6,3E" a))))
+    (T                                     (format out "~A" a))))
