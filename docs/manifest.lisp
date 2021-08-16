8b03cecc7a7e11f5c78af37854b0130c331b32fc
Nick Matvyeyev <mnasoft@gmail.com>
2021-08-16 16:54:13 +0300

    16 авг 2021 г. 13:54:12

--- docs/manifest.lisp
+++
@@ -1,8 +0,0 @@
-(:docstring-markup-format
-   :scriba
-   :systems (:MNAS-FORMAT :MNAS-FORMAT/DOCS)
-   :documents ((:title "Mnas-Format"
-	        :authors ("Nick Matvyeyev")
-	        :output-format (:TYPE :MULTI-HTML :TEMPLATE :MINIMA) 
-                :sources ("mnas-format.scr" "mnas-format-graph.scr")
-                )))
